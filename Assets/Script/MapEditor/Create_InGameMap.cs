fd3e614211b5f12548457f9c34da1f86bac7fcda
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2022-06-26 18:46:13 +0900

    인게임 맵제작
    인게임 맵제작

---
+++ Assets/Script/MapEditor/Create_InGameMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class Create_InGameMap : EditorWindow
+{
+    Object[] BoxTile;
+
+    [MenuItem("MapEditor/InGame")]
+    static void showWindow()
+    {
+        GetWindow(typeof(Create_InGameMap)).Show();
+    }
+
+    private void OnGUI()
+    {
+        BoxTile = Resources.LoadAll<Object>("Tile/InGame");
+    }
+}
