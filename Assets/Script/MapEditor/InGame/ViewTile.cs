d9d5661fca5e60a289f7c240862c962c70bf860d
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2022-07-12 17:40:09 +0900

    인게임 에디터 제작
    인게임 에디터 제작

---
+++ Assets/Script/MapEditor/InGame/ViewTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class ViewTile : MonoBehaviour
+{
+}
+
+[CustomEditor(typeof(ViewTile))]
+public class TileButton : Editor
+{
+
+    public void OnSceneGUI()
+    {
+        ViewTile Button = (ViewTile)target;
+
+        if (Handles.Button(Button.transform.position, Quaternion.AngleAxis(90, Button.transform.right), 1.0f, 2.0f, Handles.RectangleHandleCap))
+        {
+            CreateMap.GetInstance.Create(Button.transform.position);
+            Destroy(Button.gameObject);
+        }
+        Tools.current = Tool.View;
+    }
+}
